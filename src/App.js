0f8538cb1e7dc5d7b931815171da9d9737b1c900
rajemurugesh <82995662+rajemurugesh@users.noreply.github.com>
2023-08-08 15:59:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import { UserList } from "./Components/User.jsx";
+import { PostList } from "./Components/Post.jsx";
+import { PostEdit } from "./Components/EditPost.jsx";
+import { PostCreate } from "./Components/CreatePost.jsx";
+import { Dashboard } from "./Components/DashBoard.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const App = () => (
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Resource name="users" list={UserList} />
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+    />
+  </Admin>
+);
 
 export default App;
